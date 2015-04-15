509c4c1135fe917b6f4d0d11e1b7db253fcce738
Peter Bacon Darwin <pete@bacondarwin.com>
2015-04-15 14:28:01 +0100

    chore(diffs): improve diffing script

--- diffs.sh
+++ diffs.sh
@@ -1,5 +1,8 @@
-echo "-- changes in this step"
-diff -b -C 2 step-$1 step-$1-solution
+printf -v currentStep "%0*d" 2 $1
+echo ">>>>>>>>>>>> changes in step-$currentStep"
+diff -b -C 2 step-$currentStep step-$currentStep-solution
 
-echo "-- changes from last step"
-diff -b -C 2 step-$(($1-1))-solution step-$1
+let lastStepVal=$1-1
+printf -v lastStep "%0*d" 2 $lastStepVal
+echo ">>>>>>>>>>>> changes between step-$lastStep and step-$currentStep"
+diff -b -C 2 step-$lastStep-solution step-$currentStep
