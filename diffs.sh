2d9693937601e6dd4a729dc0d2f649d057431ce0
Peter Bacon Darwin <pete@bacondarwin.com>
2015-04-16 19:36:02 +0100

    diffs.sh: only show modifications not additions and removals

--- diffs.sh
+++ diffs.sh
@@ -1,8 +1,8 @@
 printf -v currentStep "%0*d" 2 $1
 echo -e "\n\n\n>>>>>>>>>>>> changes in step-$currentStep\n"
-git diff --no-index step-$currentStep step-$currentStep-solution
+git diff -D -B --no-index step-$currentStep step-$currentStep-solution
 
 let lastStepVal=$1-1
 printf -v lastStep "%0*d" 2 $lastStepVal
 echo -e "\n\n\n>>>>>>>>>>>> changes between step-$lastStep and step-$currentStep\n"
-git diff --no-index step-$lastStep-solution step-$currentStep
+git diff -D -B --no-index step-$lastStep-solution step-$currentStep
